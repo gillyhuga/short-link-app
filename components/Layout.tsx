d707accbadc4b36a6064bcb396303dc4ae89b941
gillyhuga <gilly.huga@yahoo.co.id>
2023-06-26 13:55:35 +0700

    feat: add form shorten url

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,25 +7,14 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = 'Short URL' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
-      </nav>
-    </header>
     {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
   </div>
 )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,95 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input, InputGroup, InputLeftAddon, InputRightAddon, Link } from '@chakra-ui/react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from "../store";
+import { setCustomUrl, setLoading } from '../store/shortLink';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+export default function Home() {
+  const dispatch = useDispatch();
+  const [longUrl, setLongUrl] = useState('');
+  const [slug, setSlug] = useState('');
+  const [error, setError] = useState('');
 
-export default IndexPage
+  const loading = useSelector((state: RootState) => state.shortLink.loading);
+  const customUrl = useSelector((state: RootState) => state.shortLink.customUrl);
+
+  const handleLongUrlChange = (e) => {
+    setLongUrl(e.target.value);
+    setError('');
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError('');
+    dispatch(setLoading(true));
+
+    const urlPattern = /^(https?|http):\/\/[\w.-]+\.[a-zA-Z]{2,}(\/\S*)?$/;
+    if (!urlPattern.test(longUrl)) {
+      setError('Invalid URL');
+      dispatch(setLoading(false));
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/short-links', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ slug, longUrl }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        dispatch(setCustomUrl(data.slug));
+      } else {
+        const errorData = await response.json();
+        setError(errorData.error);
+      }
+    } catch (error) {
+      setError('An error occurred');
+    }
+
+    dispatch(setLoading(false));
+  };
+
+  return (
+    <Box maxWidth="600px" mx="auto" mt={10} p={4}>
+      <form onSubmit={handleSubmit}>
+        <FormControl isInvalid={!!error}>
+          <FormLabel htmlFor="longUrl">Long URL</FormLabel>
+          <Input
+            type="text"
+            id="longUrl"
+            value={longUrl}
+            onChange={handleLongUrlChange}
+            placeholder="Enter the long URL"
+          />
+          <FormLabel htmlFor="slug" mt={4}>
+            Custom Slug
+          </FormLabel>
+          <InputGroup>
+            <InputLeftAddon children='https://url.gillyhuga.com/' />
+            <Input
+              type="text"
+              id="slug"
+              value={slug}
+              onChange={(e) => setSlug(e.target.value)}
+              placeholder="Enter a custom slug (optional)"
+            />
+          </InputGroup>
+
+          <FormErrorMessage>{error}</FormErrorMessage>
+        </FormControl>
+        <Button mt={4} colorScheme="teal" isLoading={loading} type="submit">
+          Create Short Link
+        </Button>
+      </form>
+      {customUrl && (
+        <Box mt={4} p={2} bg="gray.100">
+          <strong>Short Link:</strong> <Link href={`${window.location.origin}/${customUrl}`}>{window.location.origin}/{customUrl}</Link>
+        </Box>
+      )}
+    </Box>
+  );
+}
